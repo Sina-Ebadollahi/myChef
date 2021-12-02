bb49f6c38546f73bf9e1355c731a408e9bedb826
Sina <34670769+Sina-Ebadollahi@users.noreply.github.com>
2021-12-02 21:14:31 +0330

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdsckPjohXmlKJuV55jd45TDKAvr2f5Co",
-  authDomain: "mychef-site.firebaseapp.com",
-  projectId: "mychef-site",
-  storageBucket: "mychef-site.appspot.com",
-  messagingSenderId: "765197562653",
-  appId: "1:765197562653:web:d86f1186e0cb1d0648a7f4",
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
 };
 // initialize firebase
 firebase.initializeApp(firebaseConfig);
